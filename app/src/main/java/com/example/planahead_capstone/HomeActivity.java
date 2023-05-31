201c5a80305b586f5dc9da7a6c818b1716240cf9
jiya <jiyajoy123@gmail.com>
2023-05-31 19:23:20 -0400

    login and registration

---
+++ app/src/main/java/com/example/planahead_capstone/HomeActivity.java
@@ -0,0 +1,52 @@
+package com.example.planahead_capstone;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Bottom Navigation View
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.menu_home:
+                            // Handle the home action
+                            Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.menu_categories:
+                            // Handle the categories action
+                            Toast.makeText(HomeActivity.this, "Categories", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.menu_my_events:
+                            // Handle the my events action
+                            Toast.makeText(HomeActivity.this, "My Events", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.menu_my_account:
+                            // Handle the my account action
+                            Toast.makeText(HomeActivity.this, "My Account", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+
+                    return true;
+                }
+            };
+}
