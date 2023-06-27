bf0171ea8a38d56644de123ca3daab6ff6d9d450
SwathyNair <swathynairfeb2@gmail.com>
2023-06-26 21:32:39 -0400

    CountDown

--- app/src/main/java/com/example/planahead_capstone/EventDetailPage.java
+++ app/src/main/java/com/example/planahead_capstone/EventDetailPage.java
@@ -1,9 +1,17 @@
-
 package com.example.planahead_capstone;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class EventDetailPage extends AppCompatActivity {
 
     private ImageView addTaskImage;
+    private ImageView budgetImage;
     private TextView invitationTextView;
     private TextView eventNameTextView;
     private TextView eventLocationTextView;
@@ -31,8 +40,23 @@ public class EventDetailPage extends AppCompatActivity {
         eventDateTextView = findViewById(R.id.eventDateTextView);
         eventTimeTextView = findViewById(R.id.eventTimeTextView);
         eventBudgetTextView = findViewById(R.id.eventBudgetTextView);
-
+        budgetImage=findViewById(R.id.budgetImageView);
         databaseHelper = new DatabaseHelper(this);
+
+
+        budgetImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddBudgetPage();
+            }
+        });
+        ImageView eventOptionImage = findViewById(R.id.eventoption);
+        eventOptionImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupWindow(v);
+            }
+        });
 
         addTaskImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +70,7 @@ public class EventDetailPage extends AppCompatActivity {
             UpcomingEvent event = intent.getParcelableExtra("event");
             if (event != null) {
                 updateEventDetails(event);
+                eventId = event.getEventId();
             }
         }
     }
@@ -66,8 +91,134 @@ public class EventDetailPage extends AppCompatActivity {
         }
     }
 
+    private void showPopupWindow(View anchorView) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_menu_layout, null);
+
+        // Set up the PopupWindow
+        PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.setFocusable(true);
+        popupWindow.setElevation(10);
+
+        // Set up click listeners for popup window items
+        TextView option1TextView = popupView.findViewById(R.id.editOptionTextView);
+        TextView option2TextView = popupView.findViewById(R.id.deleteOptionTextView);
+
+        option1TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditEventPage();
+                popupWindow.dismiss();
+            }
+        });
+
+        option2TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteEvent();
+                popupWindow.dismiss();
+            }
+        });
+
+        // Calculate the x and y coordinates for the PopupWindow
+        int[] location = new int[2];
+        anchorView.getLocationOnScreen(location);
+        int anchorViewX = location[0];
+        int anchorViewY = location[1];
+
+        // Adjust the x and y coordinates to position the PopupWindow near the clicked image
+        int offsetX = anchorView.getWidth() / 2;
+        int offsetY = anchorView.getHeight() / 2;
+        int popupX = anchorViewX - offsetX;
+        int popupY = anchorViewY - offsetY;
+
+        // Show the PopupWindow
+        popupWindow.showAtLocation(anchorView, Gravity.NO_GRAVITY, popupX, popupY);
+    }
+
+    private void openEditEventPage() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            UpcomingEvent event = intent.getParcelableExtra("event");
+            if (event != null) {
+                Intent editIntent = new Intent(this, EditEventActivity.class);
+                editIntent.putExtra("event", event);
+                startActivity(editIntent);
+            }
+        }
+    }
+
+//    private void deleteEvent() {
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//
+//        // Inflate the custom dialog layout
+//        View dialogView = getLayoutInflater().inflate(R.layout.dialog_delete_event, null);
+//        builder.setView(dialogView);
+//
+//        // Find and customize the views in the custom layout
+//        TextView titleTextView = dialogView.findViewById(R.id.dialog_title);
+//        TextView messageTextView = dialogView.findViewById(R.id.dialog_message);
+//        titleTextView.setText("Delete Event");
+//        messageTextView.setText("Are you sure you want to delete this event?");
+//
+//        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                databaseHelper.deleteEvent(eventId);
+//
+//                startActivity(intent);
+//                finish();
+//                //  finish(); // Finish the activity after deleting the event
+//            }
+//        });
+//        builder.setNegativeButton("Cancel", null);
+//
+//        // Show the customized dialog
+//        AlertDialog alertDialog = builder.create();
+//        alertDialog.show();
+//    }
+//
+
+    private void deleteEvent() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogCustomStyle);
+
+        // Inflate the custom dialog layout
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_delete_event, null);
+        builder.setView(dialogView);
+
+        // Find and customize the views in the custom layout
+        TextView titleTextView = dialogView.findViewById(R.id.dialog_title);
+        TextView messageTextView = dialogView.findViewById(R.id.dialog_message);
+
+        titleTextView.setText("Delete Event");
+        messageTextView.setText("Are you sure you want to delete this event?");
+
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                databaseHelper.deleteEvent(eventId);
+                Intent intent = new Intent(EventDetailPage.this, EventsActivity.class);
+                startActivity(intent);
+                finish();
+                //  finish(); // Finish the activity after deleting the event
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+
+        // Show the customized dialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
     private void openTodoListPage() {
         Intent intent = new Intent(this, TodoListActivity.class);
         startActivity(intent);
     }
+
+    private void openAddBudgetPage() {
+        Intent intent = new Intent(this, BudgetListActivity.class);
+        startActivity(intent);
+    }
+
 }
