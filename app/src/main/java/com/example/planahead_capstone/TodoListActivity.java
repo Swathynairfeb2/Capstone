8e5369b38695e15dfd66999e4ff341698b8b6a93
SwathyNair <swathynairfeb2@gmail.com>
2023-07-06 01:11:24 -0400

    EventDetail ,TodoActivvity

--- app/src/main/java/com/example/planahead_capstone/TodoListActivity.java
+++ app/src/main/java/com/example/planahead_capstone/TodoListActivity.java
@@ -1,83 +1,3 @@
-//package com.example.planahead_capstone;
-//
-//import android.app.Activity;
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.AdapterView;
-//import android.widget.ArrayAdapter;
-//import android.widget.Button;
-//import android.widget.CheckBox;
-//import android.widget.ImageView;
-//import android.widget.ListView;
-//import android.widget.Toast;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class TodoListActivity extends Activity {
-//
-//    private ListView todoListView;
-//    private Button addTaskButton;
-//
-//    private List<TodoTask> tasks;
-//    private TodoTaskAdapter adapter;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.todo_list_page);
-//
-//        todoListView = findViewById(R.id.todoListView);
-//        addTaskButton = findViewById(R.id.addTaskButton);
-//
-//        tasks = new ArrayList<>();
-//
-//        adapter = new TodoTaskAdapter(this, tasks);
-//        todoListView.setAdapter(adapter);
-//
-//        addTaskButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Start the AddTaskActivity to add a new task
-//                Intent intent = new Intent(TodoListActivity.this, AddTaskActivity.class);
-//                startActivityForResult(intent, 1);
-//            }
-//        });
-//
-//        todoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                TodoTask task = tasks.get(position);
-//                Toast.makeText(TodoListActivity.this, "Clicked: " + task.getName(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
-//        todoListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-//            @Override
-//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-//                tasks.remove(position);
-//                adapter.notifyDataSetChanged();
-//                return true;
-//            }
-//        });
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == 1 && resultCode == RESULT_OK) {
-//            String taskName = data.getStringExtra("taskName");
-//
-//            if (taskName != null && !taskName.isEmpty()) {
-//                TodoTask todoTask = new TodoTask(taskName, false);
-//                tasks.add(todoTask);
-//                adapter.notifyDataSetChanged();
-//            }
-//        }
-//    }
-//}
 package com.example.planahead_capstone;
 
 import android.app.Activity;
@@ -109,7 +29,7 @@ public class TodoListActivity extends Activity {
         databaseHelper = new DatabaseHelper(this);
         Intent intent = getIntent();
         if (intent != null) {
-            eventId = String.valueOf(intent.getIntExtra("eventId", 0));
+            eventId = intent.getStringExtra("eventId");
 
         }
         todoListView = findViewById(R.id.todoListView);
