6a85cba6a9a58af877612e20340737c6e0fc67f9
AmalSethunath <109239250+AmalSethunath@users.noreply.github.com>
2023-07-26 07:29:26 +0530

    UserProfile details - ADD +EDIT, Change password in DB and Dark mode and light mode

--- app/src/main/java/com/example/planahead_capstone/UserAccountSettings.java
+++ app/src/main/java/com/example/planahead_capstone/UserAccountSettings.java
@@ -1,7 +1,9 @@
-
 package com.example.planahead_capstone;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -31,13 +33,17 @@ import java.io.InputStream;
 
 public class UserAccountSettings extends AppCompatActivity {
 
+    private DatabaseHelper databaseHelper;
     private EditText firstNameEditText, lastNameEditText, emailEditText, phoneEditText;
     private Button saveButton, logoutButton;
     ImageView imgChangePassword,profileImageView;
     private static final int REQUEST_IMAGE_PICK = 1;
     private SwitchCompat darkModeSwitch;
     private CustomTheme app;
-
+    private Intent intent;
+    int userId;
+    private boolean isEditMode = false;
+    private ImageView editImage;
 
 
 
@@ -45,6 +51,8 @@ public class UserAccountSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountsettings);
+        databaseHelper = new DatabaseHelper(this);
+
 
         // Initialize views
         profileImageView = findViewById(R.id.profileImage);
@@ -56,6 +64,23 @@ public class UserAccountSettings extends AppCompatActivity {
         logoutButton = findViewById(R.id.logoutButton);
         imgChangePassword = findViewById(R.id.imgChangePassword);
         darkModeSwitch = findViewById(R.id.darkModeSwitch);
+        editImage = findViewById(R.id.editImg);
+        saveButton.setVisibility(View.INVISIBLE);
+        intent = getIntent();
+        if (intent != null) {
+            userId = intent.getIntExtra("userId", userId);
+            if (userId != 0) {
+                // Fetch user data from the database and populate the UI elements
+                fetchUserDataFromDB(userId);
+                Toast.makeText(UserAccountSettings.this, "userid is :"+userId, Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(UserAccountSettings.this, "userid not found", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
 
         // Get a reference to the custom Application class
         app = (CustomTheme) getApplication();
@@ -68,13 +93,30 @@ public class UserAccountSettings extends AppCompatActivity {
         });
 
 
+        // Set a click listener on the editImg to enable edit mode
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleEditMode();
+            }
+        });
+
+        // Set onClickListener for the saveButton to save the edited data
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserDataToDB();
+            }
+        });
+
+
         // Set a listener to update the theme when the switch state changes
         darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // Update the dark mode state and apply the new theme
                 app.setDarkModeEnabled(isChecked);
-               //recreate(); // Recreate the activity to apply the theme changes
+                //recreate(); // Recreate the activity to apply the theme changes
             }
         });
 
@@ -96,6 +138,120 @@ public class UserAccountSettings extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    // Method to fetch user data from the database
+    public void fetchUserDataFromDB(int userId) {
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
+        String[] columns = {DatabaseHelper.COLUMN_FIRST_NAME, DatabaseHelper.COLUMN_LAST_NAME,
+                DatabaseHelper.COLUMN_UEMAIL, DatabaseHelper.COLUMN_UPHONE};
+        String selection = DatabaseHelper.COLUMN_UUSER_ID + " = ?";
+        String[] selectionArgs = {String.valueOf(userId)};
+        Cursor cursor = db.query(DatabaseHelper.TABLE_USER_DETAILS, columns, selection, selectionArgs, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            int fnameColumnIndex = cursor.getColumnIndex(DatabaseHelper.COLUMN_FIRST_NAME);
+            int lnameColumnIndex = cursor.getColumnIndex(DatabaseHelper.COLUMN_LAST_NAME);
+            int emailColumnIndex = cursor.getColumnIndex(DatabaseHelper.COLUMN_UEMAIL);
+            int phoneColumnIndex = cursor.getColumnIndex(DatabaseHelper.COLUMN_UPHONE);
+
+            String firstName = cursor.getString(fnameColumnIndex);
+            String lastName = cursor.getString(lnameColumnIndex);
+            String email = cursor.getString(emailColumnIndex);
+            String phone = cursor.getString(phoneColumnIndex);
+
+            // Update the EditTexts with the fetched data
+            firstNameEditText.setText(firstName);
+            lastNameEditText.setText(lastName);
+            emailEditText.setText(email);
+            phoneEditText.setText(phone);
+        }
+
+        if (cursor != null) {
+            cursor.close();
+        }
+        db.close();
+    }
+
+    private void saveUserDataToDB() {
+        // Get the edited data from the EditTexts
+        String firstName = firstNameEditText.getText().toString();
+        String lastName = lastNameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String phone = phoneEditText.getText().toString();
+
+
+        // Validate phone number
+        if (!isValidPhoneNumber(phone)) {
+            phoneEditText.setError("Invalid phone number format");
+            phoneEditText.requestFocus();
+        }
+
+        // Validate email
+        else if (!isValidEmail(email)) {
+            emailEditText.setError("Invalid email");
+            emailEditText.requestFocus();
+        } else {
+            toggleEditMode();
+
+
+            // Update the user data in the database based on the userId
+            SQLiteDatabase db = null;
+            try {
+                databaseHelper = new DatabaseHelper(this);
+                db = databaseHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put(DatabaseHelper.COLUMN_FIRST_NAME, firstName);
+                values.put(DatabaseHelper.COLUMN_LAST_NAME, lastName);
+                values.put(DatabaseHelper.COLUMN_UEMAIL, email);
+                values.put(DatabaseHelper.COLUMN_UPHONE, phone);
+
+                String whereClause = DatabaseHelper.COLUMN_UUSER_ID + " = ?";
+                String[] whereArgs = {String.valueOf(userId)};
+
+                // Check if the user ID exists in the database
+                Cursor cursor = db.query(DatabaseHelper.TABLE_USER_DETAILS, null, whereClause, whereArgs, null, null, null);
+                boolean userExists = cursor != null && cursor.getCount() > 0;
+                if (userExists) {
+                    // User ID exists, perform update
+                    db.update(DatabaseHelper.TABLE_USER_DETAILS, values, whereClause, whereArgs);
+                    Toast.makeText(UserAccountSettings.this, "User data updated successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    // User ID doesn't exist, perform insert
+                    values.put(DatabaseHelper.COLUMN_UUSER_ID, userId);
+                    db.insert(DatabaseHelper.TABLE_USER_DETAILS, null, values);
+                    Toast.makeText(UserAccountSettings.this, "User data added successfully", Toast.LENGTH_SHORT).show();
+                }
+
+                if (cursor != null) {
+                    cursor.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(UserAccountSettings.this, "Failed to update user data", Toast.LENGTH_SHORT).show();
+            } finally {
+                if (db != null) {
+                    db.close();
+                }
+            }
+        }
+    }
+
+
+    private void toggleEditMode() {
+        // Toggle the edit mode for the UI elements
+        isEditMode = !isEditMode;
+
+        firstNameEditText.setEnabled(isEditMode);
+        lastNameEditText.setEnabled(isEditMode);
+        emailEditText.setEnabled(isEditMode);
+        phoneEditText.setEnabled(isEditMode);
+
+        // Show/hide the Save button based on the edit mode
+        if (isEditMode) {
+            saveButton.setVisibility(View.VISIBLE);
+        } else {
+            saveButton.setVisibility(View.INVISIBLE);
+        }
     }
     private void showChangePasswordDialog() {
         // Create an AlertDialog builder
@@ -151,7 +307,7 @@ public class UserAccountSettings extends AppCompatActivity {
                     oldPasswordEditText.requestFocus();
                     return;
                 }
-              else if (newPassword.isEmpty()) {
+                else if (newPassword.isEmpty()) {
                     newPasswordEditText.setError("New Password cannot be empty");
                     newPasswordEditText.requestFocus();
                     return;
@@ -160,13 +316,16 @@ public class UserAccountSettings extends AppCompatActivity {
                     confirmPasswordEditText.requestFocus();
                     return;
                 }
+                if (isOldPasswordValid(oldPassword) && newPassword.equals(confirmPassword)) {
+                    // Update the password in the database
+                    if (userId != 0) {
+                        updatePasswordInDB(userId, newPassword);
+                        Toast.makeText(UserAccountSettings.this, "Password updated successfully", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(UserAccountSettings.this, "User ID not found", Toast.LENGTH_SHORT).show();
+                    }
 
-                // Add your password change logic here
-                // For example, check if the passwords match and meet your criteria
-                if (newPassword.equals(confirmPassword)) {
                     dialog.dismiss();
-
-
                     // Perform any other action after password change here
                 } else {
                     confirmPasswordEditText.setError("Passwords do not match");
@@ -175,6 +334,37 @@ public class UserAccountSettings extends AppCompatActivity {
                 }
             }
         });
+
+    }
+    // Method to check if the old password matches the one stored in the database
+    private boolean isOldPasswordValid(String oldPassword) {
+
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
+        String[] columns = {DatabaseHelper.COLUMN_PASSWORD};
+        String selection = DatabaseHelper.COLUMN_USER_ID + " = ? AND " + DatabaseHelper.COLUMN_PASSWORD + " = ?";
+        String[] selectionArgs = {String.valueOf(userId), oldPassword};
+        Cursor cursor = db.query(DatabaseHelper.TABLE_USERS, columns, selection, selectionArgs, null, null, null);
+
+        boolean isOldPasswordValid = cursor != null && cursor.getCount() > 0;
+        if (cursor != null) {
+            cursor.close();
+        }
+        db.close();
+
+        return isOldPasswordValid;
+    }
+
+    // Method to update the password in the database
+    private void updatePasswordInDB(int userId, String newPassword) {
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COLUMN_PASSWORD, newPassword);
+
+        String whereClause = DatabaseHelper.COLUMN_USER_ID + " = ?";
+        String[] whereArgs = {String.valueOf(userId)};
+
+        db.update(DatabaseHelper.TABLE_USERS, values, whereClause, whereArgs);
+        db.close();
     }
     private void showImageChangeOptionsDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -235,7 +425,18 @@ public class UserAccountSettings extends AppCompatActivity {
 
         }
     }
+    private boolean isValidEmail(String email) {
+        // Perform email validation using regular expressions
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        return email.matches(emailPattern);
+    }
 
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        // Perform phone number validation using regular expressions
+        // This example assumes that a valid phone number consists of 10 digits
+        String phoneNumberPattern = "\\d{10}";
+        return phoneNumber.matches(phoneNumberPattern);
+    }
     private int getRotationFromExif(Uri imageUri) {
         int rotation = 0;
         try {
